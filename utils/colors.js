53d5b63c6ea670f3df0a85ea4f0f2db4b94b169a
JenThomasJames <jenthomasjames123@gmail.com>
2022-11-09 17:26:41 +0530

    Configured Colors

---
+++ utils/colors.js
@@ -0,0 +1,18 @@
+const Colors = {
+  primary300: "#D9F8C4",
+  primary400: "#BCE29E",
+  primary500: "#B4FF9F",
+  primary600: "#97DBAE",
+  primary700: "#65C18C",
+  secondary100: "#FFFFFF",
+  secondary200: "#FCFCFC",
+  secondary300: "#F9F9F9",
+  secondary400: "#E9E9E9",
+  secondary500: "#B4B4B4",
+  secondary600: "#7C7C7C",
+  secondary700: "#505050",
+  muted: "#EFEFEF",
+  danger: "#E42323",
+  fuchsia: "#FF8FB1",
+};
+export default Colors;
